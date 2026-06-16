52dc71b49373f7cddd3421f5dc8539571236d203
wfr784h2gz-blip <wfr784h2gz@privaterelay.appleid.com>
2026-06-16 00:27:42 -0500

    Add dark-themed Hourly Tracker version replicating custom design - service-worker.js

---
+++ dark_hourly_tracker/service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'hourly-tracker-dark-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  '../icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) {
+        return caches.delete(key);
+      }
+    })))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request).catch(() => caches.match('./index.html')))
+  );
+});
